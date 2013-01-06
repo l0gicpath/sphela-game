0942ba84b243b1e43c9e9156b1e3bf3bea4e5221
Bjorn Tipling <bjorn@ambientchill.com>
2013-01-06 06:52:57 -0800

    remove code.

--- sphela/client/browsers.js
+++
@@ -1,9 +0,0 @@
-if (typeof requestAnimationFrame === 'undefined') {
-  var requestAnimationFrame;
-  if (typeof mozRequestAnimationFrame !== 'undefined') {
-    requestAnimationFrame = mozRequestAnimationFrame;
-  }
-  if (typeof msRequestAnimationFrame !== 'undefined') {
-    requestAnimationFrame = msRequestAnimationFrame;
-  }
-}
