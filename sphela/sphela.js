2890e6702d7fbdaf4abd6a890c9464db1956afa8
Bjorn Tipling <bjorn@ambientchill.com>
2013-01-06 06:39:57 -0800

    Meteor is buggy.

--- sphela/sphela.js
+++ sphela/sphela.js
@@ -1,134 +1,89 @@
+var Tests = new Meteor.Collection('tests');
+
 if (Meteor.isClient) {
-  $(window).ready(function() {
+  Meteor.startup(function() {
+    Session.set('testCount', 0)
     Meteor.subscribe('connect');
   });
-}
-(function() {
-  if (Meteor.isServer) {
-    Meteor.startup(function() {
-      player = Players.findOne({})
-      if (!player) {
-        player = {
-          currentRound: 0
-        };
-        player._id = Players.insert(player);
-      } else {
-        player.currentRound = 0;
-        Players.update({_id:player._id}, player);
-      }
-    });
-
-    /**
-     * Publish information when first connected.
-     */
-    Meteor.publish('connect', function() {
-      var player_;
-      player_ = Players.findOne({});
-      this.set('players', player_._id, player_);
-      this.complete();
-      this.flush();
-    });
-
-    /**
-     * Publish player updates.
-     */
-    Meteor.publish('player-game', function(round) {
-      var handle;
-      handle = Players.find({currentRound: round}).observe({
-        changed: _.bind(function(player) {
-          console.log('\n\n', player._id, player.currentRound, '\n\n');
-          this.set('players', player._id, player);
-          this.flush();
-        }, this),
-        added: _.bind(function(player) {
-          console.log('player added?');
-          this.set('players', player._id, player);
-          this.flush();
-        }, this)
-      });
-      this.complete();
-      this.flush();
-      this.onStop(function() {
-        handle.stop();
-      });
-    });
-
-  }
-  })();
-  (function() {
-  Meteor.methods({
-    /**
-     * When a user joins the round the client calls this method.
-     */
-    joinRound: function() {
-      var player, playerRound;
-      console.log('joining');
-      player = Players.findOne({});
-      if (player.currentRound !== 1) {
-        console.log('updating player round', player, 1);
-        player.currentRound = 1;
-        Players.update({_id: player._id}, player);
-      } else {
-        console.log('not updating player round', player, 1);
-      }
-      console.log('join done', Players.findOne());
-    },
-  });
-})();
-
-var Players;
-
-function startRound() {
-}
-
-/**
- * Players store additional information for users.
- * @type {Meteor.Collection}
- */
-Players = new Meteor.Collection('players');
-
-(function() {
-  if (Meteor.isClient) {
-    var currentRoundSub;
-
-    /**
-     * @param {Object} event
-     */
-    function joinRound(event) {
-      Meteor.call('joinRound');
-      Session.set('currentRound', 1);
+  Template.app.testSuccess = function() {
+    var test;
+    console.log('checking test result');
+    test = Tests.findOne();
+    if (!test) {
+      console.log('no test', test);
+      return false;
     }
-
-
-    /**
-     * @return {boolean}
-     */
-    Template.playerStatus.isPlaying = function() {
-      var player;
-      console.log('is playering?');
-      player = Players.findOne();
-      if (!player) {
-        console.log('no player', player);
-        return false;
-      }
-      console.log('player.currentRound', player.currentRound);
-      return player.currentRound > 0;
-    };
-
-    Template.playerStatus.events({
-      'click .join-round': joinRound
-    });
-
-    Meteor.autorun(function() {
-      console.log('Players updated!', Players.findOne());
-    });
-
-    Meteor.autosubscribe(function() {
-    Meteor.subscribe('player-game', Session.get('currentRound'));
-    });
-    Meteor.autosubscribe(function() {
-      console.log('round updates', Session.get('currentRound'));
-      Meteor.subscribe('player-round-updates', Session.get('currentRound'));
-    });
+    console.log('test.testCount', test.testCount);
+    return test.testCount > 0;
+  };
+  Template.app.events({
+    'click .run-test': runTest
+  });
+  function runTest(event) {
+    Meteor.call('runTest');
+    Session.set('testCount', 1);
   }
-})();
+  Meteor.autorun(function() {
+    console.log('Tests updated!', Tests.findOne());
+  });
+  Meteor.autosubscribe(function() {
+    Meteor.subscribe('test-results', Session.get('testCount'));
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function() {
+    test = Tests.findOne({})
+    if (!test) {
+      test = {
+        testCount: 0
+      };
+      console.log('inserting new test');
+      test._id = Tests.insert(test);
+    } else {
+      console.log('startup reset');
+      test.testCount = 0;
+      Tests.update({_id:test._id}, test);
+    }
+  });
+
+  Meteor.publish('connect', function() {
+    var test_;
+    console.log('Connecting test, getting initial data.');
+    test_ = Tests.findOne({});
+    this.set('tests', test_._id, test_);
+    this.complete();
+    this.flush();
+  });
+
+  Meteor.publish('test-results', function(test) {
+    var handle;
+    handle = Tests.find({testCount: test}).observe({
+      changed: _.bind(function(test) {
+        console.log('Test changed', test._id, test.testCount);
+        this.set('tests', test._id, test);
+        this.flush();
+      }, this),
+      added: _.bind(function(test) {
+        console.log('Test added do nothing', test);
+      }, this)
+    });
+    this.complete();
+    this.flush();
+    this.onStop(function() {
+      handle.stop();
+    });
+  });
+}
+Meteor.methods({
+  runTest: function() {
+    var test;
+    console.log('Running test.');
+    test = Tests.findOne({});
+    test.testCount = 1;
+    console.log('Updating test to 1.');
+    Tests.update({_id: test._id}, test);
+    console.log('Test ran.', Tests.findOne());
+  },
+});
+
